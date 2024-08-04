7300b826e8ac783c632a2f0da727055b947c334c
chrisaharden <68924870+chrisaharden@users.noreply.github.com>
2024-08-03 21:45:46 -0400

    Adding examples in README and a test.cmd file
    Adding examples in README and a test.cmd file

--- test.cmd
+++ test.cmd
@@ -1,6 +1,3 @@
-# ollamagamecards
-
-Example Usage: (see test.cmd)
 python .\maincards.py "Generate a pdf card deck with 50 unique funny objects with a title of Objects and use Footlight MT Light font"
 python .\maincards.py "Generate a pdf card deck with 50 unique verbs with a title of Verbs and use Comic Sans MS font"
 python .\maincards.py "Generate a pdf card deck with 50 unique stinky smells with a title of Smells and use Informal Roman font"
